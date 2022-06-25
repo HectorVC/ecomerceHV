051cfa1059217f56bdf252ae5b5527cadb1e2a19
Hector Viveros <true_blood1009@hotmail.com>
2022-06-24 20:37:35 -0500

    carpeta navbar

---
+++ ecomerce-npx/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+const NavBar =() =>{
+return(
+<div class="container-fluid bg-dark text-white p-0">
+    <div class="row gx-0 d-none d-lg-flex">
+      <div class="col-lg-7 px-5 text-start">
+        <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+          <small class="fa fa-map-marker-alt text-danger me-2"></small>
+          <small>#4124 Calle X , Puebla, Mexico</small>
+        </div>
+              </div>
+      <div class="col-lg-5 px-5 text-end">
+        <div class="h-100 d-inline-flex align-items-center py-3 me-4">
+          <small class="fa fa-phone-alt text-danger me-2"></small>
+          <small>+222 222 222</small>
+        </div>
+        <div class="h-100 d-inline-flex align-items-center">
+          <a class="btn btn-sm-square bg-white text-danger me-1" href=""><i class="fab fa-facebook-f"></i></a>
+          <a class="btn btn-sm-square bg-white text-danger me-1" href=""><i class="fab fa-twitter"></i></a>
+          <a class="btn btn-sm-square bg-white text-danger me-1" href=""><i class="fab fa-linkedin-in"></i></a>
+          <a class="btn btn-sm-square bg-white text-danger me-0" href=""><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+}
