0eddfcf01dcc125b7d01185cf3f49358034afc88
Hector Viveros <true_blood1009@hotmail.com>
2022-06-24 21:17:39 -0500

    agregue pagina

--- ecomerce-npx/src/components/container/ItemListContainer.jsx
+++ ecomerce-npx/src/components/container/ItemListContainer.jsx
@@ -1,17 +1,27 @@
 const ItemListContainer = () => {
     return (
      <>
-     <div class="container">
+     <div class="container-fluid p-0 pb-5">
+    <div class="owl-carousel header-carousel position-relative">
+      <div class="owl-carousel-item position-relative">
+        <img class="img-fluid" src="img/inicio.png" alt=""/>
+        <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
+          style="background: rgba(53, 53, 53, .7);">
+          <div class="container">
             <div class="row justify-content-center">
               <div class="col-12 col-lg-8 text-center">
-                <h6 class="text-black text-uppercase mb-3 animated slideInDown">Cuidando la industria</h6>
-                <h1 class="display-3 text-black animated slideInDown mb-4">HV COMPRESSORS</h1>
-                <p class="fs-5 fw-medium text-black mb-4 pb-2">Mantenimiento especializado en compresores industriales.</p>
+                <h6 class="text-white text-uppercase mb-3 animated slideInDown">Cuidando la industria</h6>
+                <h1 class="display-3 text-white animated slideInDown mb-4">HV COMPRESSORS</h1>
+                <p class="fs-5 fw-medium text-white mb-4 pb-2">Mantenimiento especializado en compresores industriales.</p>
                 <a href="pages/contacto.html" class="btn btn-danger py-md-3 px-md-5 me-3 animated slideInLeft">Contactanos</a>
                 <a href="#Info" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Mas informacion</a>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+  </div>
      </>
         );
  };
