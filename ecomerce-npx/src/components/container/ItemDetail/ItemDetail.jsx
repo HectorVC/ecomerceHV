d7fe22557b41b17ba1c3565756fcd8fe37f45802
Hector Viveros <true_blood1009@hotmail.com>
2022-07-11 17:59:28 -0500

    agrego

--- ecomerce-npx/src/components/container/ItemDetail/ItemDetail.jsx
+++ ecomerce-npx/src/components/container/ItemDetail/ItemDetail.jsx
@@ -5,7 +5,20 @@ const ItemDetail = () => {
 const {id} = useParams();
 console.log (id)
   return (
-    <div>ItemDetail</div>
+    <div className="card mb-3 w-50">
+  <div className="row g-0">
+    <div className="col-md-4">
+      <img src="..." className="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
   )
 }
 
