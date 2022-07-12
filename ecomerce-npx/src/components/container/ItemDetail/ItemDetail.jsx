00061d0bf703c297adf62ace39e6d064c47e40aa
Hector Viveros <true_blood1009@hotmail.com>
2022-07-12 12:12:50 -0500

    agrego detalles

--- ecomerce-npx/src/components/container/ItemDetail/ItemDetail.jsx
+++ ecomerce-npx/src/components/container/ItemDetail/ItemDetail.jsx
@@ -1,25 +1,60 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+import { gFetch } from "../helpers/getFetch";
 
-const ItemDetail = () => {
-const {id} = useParams();
-console.log (id)
-  return (
-    <div className="card mb-3 w-50">
-  <div className="row g-0">
-    <div className="col-md-4">
-      <img src="..." className="img-fluid rounded-start" alt="..."/>
-    </div>
-    <div className="col-md-8">
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+// const ItemDetail = () => {
+// const {id} = useParams();
+// console.log (id)
+//   return (
+//     <div className="card mb-3 w-50">
+//   <div className="row g-0">
+//     <div className="col-md-4">
+//       <img src="..." className="img-fluid rounded-start" alt="..."/>
+//     </div>
+//     <div className="col-md-8">
+//       <div className="card-body">
+//         <h5 className="card-title">SABOR DEL BOLI</h5>
+//         <p className="card-text">Descripcion</p>
+//         <p className="card-text"><small className="text-muted">Precio por unidad : $ </small></p>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+//   )
+// }
+
+// export default ItemDetail
+
+const ItemDetail = ({ greeting }) => {
+  const [productos, setProductos] = useState([]);
+  
+
+  useEffect(() => {
+    gFetch
+      .then((resp) => setProductos(resp))
+      .catch((err) => console.log(err))
+        }, []);
+
+// const ItemDetail = () => {
+//   const [productos, setProductos] = useState([]);
+  return productos.map((prod) => (
+    <div key={prod.id} className="card mb-3 w-50">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src="..." className="img-fluid rounded-start" alt="..." />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{prod.sabor}</h5>
+            <p className="card-text">Rico Boli Gourmet a base de {prod.sabor} y {prod.base} {prod.des} {prod.sabor}</p>
+            <p className="card-text">
+              <small className="text-muted">Precio por unidad : $ </small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-  )
-}
+  ));
+};
 
-export default ItemDetail
+export default ItemDetail;
--- ecomerce-npx/src/components/container/data/stock.js
+++ ecomerce-npx/src/components/container/data/stock.js
@@ -1,8 +1,8 @@
 import imagenes from "../img/imagenes"
 export const productos = [
-    {id:'1',sabor:'Oreo',precio:'15',base:'Leche',img:imagenes.img1},
-    {id:'2',sabor:'Chocolate',precio:'15',base:'Leche',img:imagenes.img2},
-    {id:'3',sabor:'Rompope',precio:'20',base:'Agua',img:imagenes.img3},
-    {id:'4',sabor:'Coco',precio:'15',base:'Agua',img:imagenes.img4},
+    {id:'1',sabor:'Oreo',precio:'15',base:'Leche',img:imagenes.img1,des:'con trozos de Galleta'},
+    {id:'2',sabor:'Chocolate',precio:'15',base:'Leche',img:imagenes.img2,des:'con pedazos de'},
+    {id:'3',sabor:'Rompope',precio:'20',base:'Leche',img:imagenes.img3,des:''},
+    {id:'4',sabor:'Coco',precio:'15',base:'Agua',img:imagenes.img4,des:'con trozos de'},
     
     ];
